e3ee2044dac131d9bdc430cdde8fef6dc79af756
MaybeMaru <97055307+MaybeMaru@users.noreply.github.com>
2024-02-18 22:16:19 +0100

    Update Script.hx

--- hscript/Script.hx
+++ hscript/Script.hx
@@ -3,38 +3,53 @@ import hscript.Parser;
 import hscript.Interp;
 
 class Script {
-    public static var parser:Parser = new Parser();
+	public static var parser:Parser = new Parser();
 	public var interp:Interp;
-    public var variables(get, never):Map<String, Dynamic>;
-	public function get_variables() return interp.variables;
 
-    public function new() {
-        interp  = new Interp();
-    }
+	public var variables(get, never):Map<String, Dynamic>;
+	inline function get_variables()
+		return interp.variables;
 
-    public function executeString(code:String = "") {
-        if (code.length <= 0) return null;
+	public function new() {
+		interp  = new Interp();
+	}
+
+	public function executeString(code:String = "") {
+		if (code.length <= 0)
+			return null;
+		
 		parser.line = 1;
 		parser.allowTypes = true;
-        parser.allowJSON = true;
-        parser.allowMetadata = true;
+		parser.allowJSON = true;
+		parser.allowMetadata = true;
+		
 		return interp.execute(parser.parseString(code));
-    }
+	}
 
 	inline public function set(varName:String, varValue:Dynamic):Void {
 		interp.setVar(varName, varValue);
 	}
 
 	inline public function get(field:String):Dynamic {
-		return interp.variables.get(field);
+		return variables.get(field);
 	}
 
 	inline public function exists(field:String):Bool {
-		return interp.variables.exists(field);
+		return variables.exists(field);
 	}
 
-    inline public function call(method:String, ?args:Array<Dynamic>):Dynamic {
-        if (!exists(method)) return null;
-        return Reflect.callMethod(this, get(method), args == null ? [] : args);
+	static final EMPTY_ARGUMENTS:Array<Dynamic> = [];
+
+	inline public function call(method:String, ?args:Array<Dynamic>):Dynamic {
+		final method = get(method);
+
+		if (method == null) return null;
+		else
+		{
+			if (args == null)
+				args = EMPTY_ARGUMENTS;
+
+			return Reflect.callMethod(this, get(method), args);
+		}
 	}
 }
